76fc9c918b7d41494161e85dcb59a925d472b958
Christian Nagel <christian@christiannagel.com>
2022-07-13 22:31:43 +0200

    DI container configuration 17

--- src/xaml/CodeBreaker.MAUI/MauiProgram.cs
+++ src/xaml/CodeBreaker.MAUI/MauiProgram.cs
@@ -1,4 +1,10 @@
-﻿namespace CodeBreaker.MAUI;
+﻿using CommunityToolkit.Maui;
+using CodeBreaker.ViewModels;
+using CodeBreaker.ViewModels.Services;
+using CodeBreaker.MAUI.Services;
+using CodeBreaker.Services;
+
+namespace CodeBreaker.MAUI;
 
 public static class MauiProgram
 {
@@ -7,12 +13,20 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-
+		builder.Services.Configure<CodeBreaker6x4ViewModelOptions>(
+			options => options.EnableDialogs = false);
+		builder.Services.AddTransient<CodeBreaker6x4ViewModel>();
+		builder.Services.AddScoped<IDialogService, MauiDialogService>();
+		builder.Services.AddHttpClient<GameClient>(options =>
+		{
+			options.BaseAddress = new("https://codebreakerapi.purplebush-9a246700.westeurope.azurecontainerapps.io");
+		});
 		return builder.Build();
 	}
 }
---
+++ src/xaml/CodeBreaker.MAUI/Services/MauiDialogService.cs
@@ -0,0 +1,11 @@
+﻿using CodeBreaker.ViewModels.Services;
+
+namespace CodeBreaker.MAUI.Services;
+
+public class MauiDialogService : IDialogService
+{
+    public Task ShowMessageAsync(string message)
+    {
+        return Task.CompletedTask;
+    }
+}
