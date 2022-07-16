9b9eaef4f039798954cd42c22cb0a1a7029ea035
Christian Nagel <christian@christiannagel.com>
2022-07-16 14:15:12 +0200

    coding guidelines .NET MAUI

--- src/xaml/CodeBreaker.MAUI/Resources/Styles/CodebreakerStyles.xaml.cs
+++ src/xaml/CodeBreaker.MAUI/Resources/Styles/CodebreakerStyles.xaml.cs
@@ -2,8 +2,8 @@ namespace CodeBreaker.MAUI.Resources.Styles;
 
 public partial class CodebreakerStyles : ResourceDictionary
 {
-	public CodebreakerStyles()
-	{
-		InitializeComponent();
-	}
+    public CodebreakerStyles()
+    {
+        InitializeComponent();
+    }
 }
--- src/xaml/CodeBreaker.MAUI/Views/PegSelectionView.xaml.cs
+++ src/xaml/CodeBreaker.MAUI/Views/PegSelectionView.xaml.cs
@@ -2,9 +2,8 @@ namespace CodeBreaker.MAUI.Views;
 
 public partial class PegSelectionView : ContentView
 {
-	public PegSelectionView()
-	{
-		InitializeComponent();
-	}
-
+    public PegSelectionView()
+    {
+        InitializeComponent();
+    }
 }
