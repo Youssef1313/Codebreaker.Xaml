87ff04bbbf178fe2ddf88b1f2089236b781b5733
Sebastian <10291845+szv@users.noreply.github.com>
2022-09-16 14:47:26 +0200

    Cleaned up WinUI

--- src/clients/xaml/CodeBreaker.WinUI/Converters/SelectionAndKeyPegToSelectionBrushConverter.cs
+++ src/clients/xaml/CodeBreaker.WinUI/Converters/SelectionAndKeyPegToSelectionBrushConverter.cs
@@ -25,7 +25,6 @@ public class SelectionAndKeyPegToSelectionBrushConverter : IValueConverter
     public Brush GreenBrush { get; set; } = s_greenBrush;
     public Brush BlueBrush { get; set; } = s_blueBrush;
     public Brush YellowBrush { get; set; } = s_yellowBrush;
-
     private Brush EmptyBrush { get; set; } = s_emptyBrush;
 
     public object Convert(object value, Type targetType, object parameter, string language)
@@ -46,27 +45,6 @@ public class SelectionAndKeyPegToSelectionBrushConverter : IValueConverter
             Yellow => YellowBrush,
             _ => EmptyBrush
         };
-
-        //ArgumentNullException.ThrowIfNull(parameter);
-        //if (value is SelectionAndKeyPegs selection)
-        //{
-        //    int ix = int.Parse(parameter.ToString()!);
-
-        //    return selection.GuessPegs[ix] switch
-        //    {
-        //        Black => BlackBrush,
-        //        White => WhiteBrush,
-        //        Red => RedBrush,
-        //        Green => GreenBrush,
-        //        Blue => BlueBrush,
-        //        Yellow => YellowBrush,
-        //        _ => EmptyBrush
-        //    };
-        //}
-        //else
-        //{
-        //    throw new ArgumentException("value is not of type SelectionAndKeyPegs");
-        //}
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, string language)
