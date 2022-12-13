6aa5a2090f0190e9eeb87b947acd5e9eaf8260be
Sebastian <10291845+szv@users.noreply.github.com>
2022-12-14 00:18:57 +0100

    Added CustomAttachedProperty for simplifying the use of icons in the NavigationViewItem

---
+++ src/clients/xaml/CodeBreaker.WinUI/CustomAttachedProperties/NavigationViewItemHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml.Media;
+
+namespace CodeBreaker.WinUI.CustomAttachedProperties;
+
+public class NavigationViewItemHelper : DependencyObject
+{
+    public static readonly DependencyProperty IconGlyphProperty =
+        DependencyProperty.RegisterAttached(
+            "IconGlyph",
+            typeof(string),
+            typeof(NavigationViewItemHelper),
+            new(string.Empty)
+        );
+
+    public static void SetIconGlyph(NavigationViewItem navigationViewItem, string value)
+    {
+        navigationViewItem.Icon = new FontIcon()
+        {
+            FontFamily = navigationViewItem.Resources["SymbolThemeFontFamily"] as FontFamily,
+            Glyph = value
+        };
+    }
+
+    public static string GetIconGlyph(NavigationViewItem navigationViewItem)
+    {
+        if (navigationViewItem.Icon is not FontIcon fontIcon)
+            return string.Empty;
+
+        return fontIcon.Glyph ?? string.Empty;
+    }
+}
