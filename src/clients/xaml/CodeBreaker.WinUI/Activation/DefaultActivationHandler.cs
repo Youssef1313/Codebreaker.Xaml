52eddbe59e7933b4f46212321e84e7cfe33470f8
Sebastian <10291845+szv@users.noreply.github.com>
2022-12-16 17:03:19 +0100

    Adapted DefaultActivationHandler for navigation to AuthPage or GamePage depending on the login-state.

--- src/clients/xaml/CodeBreaker.WinUI/Activation/DefaultActivationHandler.cs
+++ src/clients/xaml/CodeBreaker.WinUI/Activation/DefaultActivationHandler.cs
@@ -1,4 +1,5 @@
-﻿using CodeBreaker.ViewModels;
+﻿using CodeBreaker.Services.Authentication;
+using CodeBreaker.ViewModels;
 using CodeBreaker.WinUI.Contracts.Services;
 
 namespace CodeBreaker.WinUI.Activation;
@@ -7,9 +8,12 @@ public class DefaultActivationHandler : ActivationHandler<LaunchActivatedEventAr
 {
     private readonly INavigationService _navigationService;
 
-    public DefaultActivationHandler(INavigationService navigationService)
+    private readonly IAuthService _authService;
+
+    public DefaultActivationHandler(INavigationService navigationService, IAuthService authService)
     {
         _navigationService = navigationService;
+        _authService = authService;
     }
 
     protected override bool CanHandleInternal(LaunchActivatedEventArgs? args)
@@ -20,7 +24,12 @@ public class DefaultActivationHandler : ActivationHandler<LaunchActivatedEventAr
 
     protected override Task HandleInternalAsync(LaunchActivatedEventArgs? args)
     {
-        _navigationService.NavigateToViewModel(typeof(AuthPageViewModel), args?.Arguments);
+        Type pageType = _authService.IsAuthenticated
+            ? typeof(GamePageViewModel)
+            : typeof(AuthPageViewModel);
+
+        _navigationService.NavigateToViewModel(pageType, args?.Arguments);
+
         return Task.CompletedTask;
     }
 }
