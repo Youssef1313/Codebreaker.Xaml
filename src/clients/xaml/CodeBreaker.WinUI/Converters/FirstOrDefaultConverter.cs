689f22580674a1f9199626c1b24f70c39727d0da
Sebastian <10291845+szv@users.noreply.github.com>
2023-03-06 23:41:45 +0100

    Added user-information to the account-page

---
+++ src/clients/xaml/CodeBreaker.WinUI/Converters/FirstOrDefaultConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Microsoft.UI.Xaml.Data;
+
+namespace CodeBreaker.WinUI.Converters;
+
+internal class FirstOrDefaultConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object parameter, string language)
+    {
+        if (value is not IEnumerable enumerable)
+            throw new ArgumentException("The value has to be an enumerable");
+
+        foreach (var item in enumerable)
+            return item;
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        throw new NotImplementedException();
+}
