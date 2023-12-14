ac3b0b6589fbcbcf0503bdac31b43fdcc20dc942
Sebastian Szvetecz <sebastian@szvetecz.at>
2023-12-14 21:12:23 +0100

    WPF: Added basic navigation-service

--- src/Codebreaker.WPF/App.xaml.cs
+++ src/Codebreaker.WPF/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Codebreaker.WPF;
+﻿using Codebreaker.ViewModels.Contracts.Services;
+using Codebreaker.WPF.Contracts;
+using Codebreaker.WPF.Services;
+
+namespace Codebreaker.WPF;
 
 /// <summary>
 /// Interaction logic for App.xaml
@@ -24,7 +28,8 @@ public sealed partial class App : Application, IDisposable
             .ConfigureServices((context, services) =>
             {
                 services.Configure<GamePageViewModelOptions>(options => options.EnableDialogs = true);
-                services.AddTransient<IDialogService, Services.WPFDialogService>();
+                services.AddSingleton<IWPFNavigationService, WPFNavigationService>();
+                services.AddSingleton<INavigationService>(x => x.GetRequiredService<IWPFNavigationService>());
                 services.AddScoped<GamePageViewModel>();
                 services.AddHttpClient<IGamesClient, GamesClient>(client =>
                 {
@@ -52,7 +57,9 @@ public sealed partial class App : Application, IDisposable
         DefaultScope.ServiceProvider.GetRequiredService<T>();
 
     protected override void OnActivated(EventArgs e)
+    protected override async void OnActivated(EventArgs e)
     {
         base.OnActivated(e);
+        await GetService<IWPFNavigationService>().NavigateToAsync("GamePage");
     }
 }
---
+++ src/Codebreaker.WPF/Services/WPFNavigationService.cs
@@ -0,0 +1,43 @@
+﻿using Codebreaker.WPF.Contracts;
+using Codebreaker.WPF.Views.Pages;
+
+namespace Codebreaker.WPF.Services;
+
+internal class WPFNavigationService : IWPFNavigationService
+{
+    private Frame? _frame;
+
+    public Frame Frame
+    {
+        get
+        {
+            if (_frame is null)
+                _frame = App.Current.MainWindow.Content as Frame ?? throw new InvalidOperationException("MainWindow content is not a frame");
+
+            return _frame;
+        }
+        set
+        {
+            _frame = value;
+        }
+    }
+
+    public bool CanGoBack { get; }
+
+    public bool GoBack()
+    {
+        if (Frame.CanGoBack || _frame is null)
+            return false;
+
+        Frame.GoBack();
+        return true;
+    }
+
+    public ValueTask<bool> GoBackAsync() =>
+        ValueTask.FromResult(GoBack());
+
+    public ValueTask<bool> NavigateToAsync(string key, object? parameter = null, bool clearNavigation = false)
+    {
+        return ValueTask.FromResult(Frame.Navigate(new GamePage())); // TODO Replace with page from pageService
+    }
+}
