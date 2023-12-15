9a547a278450825606fe88942b7a85ebc2c87c3a
Sebastian Szvetecz <sebastian@szvetecz.at>
2023-12-15 04:00:39 +0100

    WPF: Added InfoBar

---
+++ src/Codebreaker.WPF/Views/Components/InfoBarArea.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Codebreaker.ViewModels.Contracts.Services;
+
+namespace Codebreaker.WPF.Views.Components;
+
+/// <summary>
+/// Interaction logic for InfoBarArea.xaml
+/// </summary>
+public partial class InfoBarArea : UserControl
+{
+    public InfoBarArea()
+    {
+        ViewModel = App.Current.GetService<IInfoBarService>();
+        InitializeComponent();
+        DataContext = this;
+        ViewModel.New
+            .WithTitle("Test")
+            .WithMessage("TestMessage")
+            .WithSeverity(ViewModels.Components.InfoMessageSeverity.Success)
+            .Show();
+    }
+
+    public IInfoBarService ViewModel { get; }
+}
