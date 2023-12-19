1040ee9ffa9403b1c1b226b87366d37c68d4eeb0
Sebastian Szvetecz <sebastian@szvetecz.at>
2023-12-19 14:22:37 +0100

    Added connected animation to game page

--- src/Codebreaker.WinUI/Views/Components/PegSelectionComponent.xaml.cs
+++ src/Codebreaker.WinUI/Views/Components/PegSelectionComponent.xaml.cs
@@ -1,12 +1,16 @@
 using Codebreaker.ViewModels;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml.Media.Animation;
 
 namespace CodeBreaker.WinUI.Views.Components;
 
-internal sealed partial class PegSelectionComponent : UserControl
+internal sealed partial class PegSelectionComponent : UserControl, IRecipient<GameMoveMessage>
 {
     public PegSelectionComponent()
     {
         InitializeComponent();
+        WeakReferenceMessenger.Default.Register(this);
+        WeakReferenceMessenger.Default.UnregisterAllOnUnloaded(this);
     }
 
     public GamePageViewModel ViewModel
@@ -22,4 +26,13 @@ internal sealed partial class PegSelectionComponent : UserControl
     public static readonly DependencyProperty ViewModelProperty =
         DependencyProperty.Register("ViewModel", typeof(GamePageViewModel), typeof(PegSelectionComponent), new PropertyMetadata(null));
 
+    public void Receive(GameMoveMessage message)
+    {
+        if (message.GameMoveValue is not GameMoveValue.Started)
+            return;
+
+        var animationService = ConnectedAnimationService.GetForCurrentView();
+        this.FindItemsOfType<ComboBox>(this)
+            .Foreach((comboBox, i) => animationService.PrepareToAnimate($"guess{i}", comboBox));
+    }
 }
