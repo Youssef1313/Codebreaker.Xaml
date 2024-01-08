bfa36e2482558372ab432d3bccb8dfc719ce9791
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-08 04:40:12 +0100

    Renamed MainPage to ShellPage and moved it to Views/Pages

--- src/Codebreaker.Uno/CodebreakerUno/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CodebreakerUno;
-
-public sealed partial class MainPage : Page
-{
-    public MainPage()
-    {
-        this.InitializeComponent();
-    }
-}
---
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Pages/ShellPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CodebreakerUno.Views.Pages;
+
+public sealed partial class ShellPage : Page
+{
+    public ShellPage()
+    {
+        InitializeComponent();
+    }
+}
