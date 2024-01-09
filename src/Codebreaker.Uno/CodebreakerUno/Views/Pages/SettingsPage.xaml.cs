fe47e8660ce150758739dad33a19e079e1f31f9d
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-09 04:48:58 +0100

    Added empty SettingsPage

---
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+namespace CodebreakerUno.Views.Pages;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class SettingsPage : Page
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
