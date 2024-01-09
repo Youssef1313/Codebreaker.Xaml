32e499415b15ecd5ce8189fb309996fc08ea7bcc
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-09 04:48:35 +0100

    Added empty GamePage

---
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Pages/GamePage.xaml.cs
@@ -0,0 +1,12 @@
+namespace CodebreakerUno.Views.Pages;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class GamePage : Page
+{
+    public GamePage()
+    {
+        InitializeComponent();
+    }
+}
