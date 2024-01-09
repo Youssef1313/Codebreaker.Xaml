b5dd315ab4ec837c22ee62ceee6dc236143ef1fb
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-09 04:49:54 +0100

    Linked just implemented vm logic to ShellPage

--- src/Codebreaker.Uno/CodebreakerUno/Views/Pages/ShellPage.xaml.cs
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Pages/ShellPage.xaml.cs
@@ -10,9 +10,26 @@ public sealed partial class ShellPage : Page
         ViewModel = viewModel;
         InitializeComponent();
 
+        ViewModel.NavigationService.Frame = NavigationFrame;
+        ViewModel.NavigationViewService.Initialize(NavigationViewControl);
+
         App.Current.MainWindow!.ExtendsContentIntoTitleBar = true;
         AppTitleBarText.Text = "AppDisplayName".GetLocalized();
     }
 
     public ShellViewModel ViewModel { get; }
+
+    private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
+    {
+        AppTitleBar.Margin = new Thickness()
+        {
+            Left = sender.CompactPaneLength * (sender.DisplayMode == NavigationViewDisplayMode.Minimal ? 2 : 1),
+            Top = AppTitleBar.Margin.Top,
+            Right = AppTitleBar.Margin.Right,
+            Bottom = AppTitleBar.Margin.Bottom
+        };
+
+        if (sender.DisplayMode != NavigationViewDisplayMode.Expanded)
+            NavigationViewControl.Header = null;
+    }
 }
