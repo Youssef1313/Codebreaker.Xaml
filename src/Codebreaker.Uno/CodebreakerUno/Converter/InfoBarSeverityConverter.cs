79fe7a66ca00ef047678a4a57e5260def03f7bc3
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-13 02:31:48 +0100

    Added GamePage functionality

--- src/Codebreaker.Uno/CodebreakerUno/Converter/InfoBarSeverityConverter.cs
+++ src/Codebreaker.Uno/CodebreakerUno/Converter/InfoBarSeverityConverter.cs
@@ -3,7 +3,7 @@ using Microsoft.UI.Xaml.Data;
 
 namespace CodebreakerUno.Converters;
 
-public class InfoBarSeverityConverter : IValueConverter
+internal class InfoBarSeverityConverter : IValueConverter
 {
     public object? Convert(object value, Type targetType, object parameter, string language) =>
         value switch
--- src/Codebreaker.Uno/CodebreakerUno/Views/Pages/GamePage.xaml.cs
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Pages/GamePage.xaml.cs
@@ -1,19 +1,44 @@
-using Codebreaker.ViewModels.Contracts.Services;
+using Codebreaker.ViewModels;
+using CodebreakerUno.Helpers;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Shapes;
 
 namespace CodebreakerUno.Views.Pages;
 
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class GamePage : Page
+public sealed partial class GamePage : Page, IRecipient<GameMoveMessage>
 {
     public GamePage()
     {
+        ViewModel = App.Current.GetService<GamePageViewModel>();
         InitializeComponent();
     }
 
-    private void ShowDemoInfoMessage(object sender, RoutedEventArgs e)
+    public GamePageViewModel ViewModel { get; }
+
+    public void Receive(GameMoveMessage message)
     {
-        App.Current.GetService<IInfoBarService>().New.WithMessage("Hello there").Show();
+        if (message.GameMoveValue is not GameMoveValue.Completed)
+            return;
+
+        var selectionAndKeyPegs = message.SelectionAndKeyPegs ?? throw new InvalidOperationException();
+        var animationService = ConnectedAnimationService.GetForCurrentView();
+        animationService.DefaultDuration = TimeSpan.FromMilliseconds(500);
+        var container = listGameMoves.ItemContainerGenerator.ContainerFromItem(selectionAndKeyPegs);
+        this.FindItemsOfType<Ellipse>(container)
+            .ForEach((i, ellipse) =>
+            {
+                ConnectedAnimation? animation = animationService.GetAnimation($"guess{i}");
+
+                // No animation found for this ellipxe -> the ellipse is most likely a key-peg
+                if (animation is null)
+                    return;
+
+                animation.Configuration = new BasicConnectedAnimationConfiguration();
+                animation.TryStart(ellipse);
+            });
     }
 }
