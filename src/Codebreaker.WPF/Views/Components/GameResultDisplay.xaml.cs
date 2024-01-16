f5c8b8249a713d3734ea51e771b8369071366d21
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-16 03:47:46 +0100

    Replaced all dependency properties with one vm dependency property

--- src/Codebreaker.WPF/Views/Components/GameResultDisplay.xaml.cs
+++ src/Codebreaker.WPF/Views/Components/GameResultDisplay.xaml.cs
@@ -1,14 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
-
-namespace Codebreaker.WPF.Views.Components;
+﻿namespace Codebreaker.WPF.Views.Components;
 
 /// <summary>
 /// Interaction logic for GameResultDisplay.xaml
 /// </summary>
-[ObservableObject]
 public partial class GameResultDisplay : UserControl
 {
     public GameResultDisplay()
@@ -16,59 +10,12 @@ public partial class GameResultDisplay : UserControl
         InitializeComponent();
     }
 
-    public GameMode GameMode
+    public GamePageViewModel ViewModel
     {
-        get => (GameMode)GetValue(GameModeProperty);
-        set
-        {
-            SetValue(GameModeProperty, value);
-            Debug.WriteLine(value);
-            OnPropertyChanged(nameof(GameMode));
-        }
+        get { return (GamePageViewModel)GetValue(ViewModelProperty); }
+        set { SetValue(ViewModelProperty, value); }
     }
 
-    public static readonly DependencyProperty GameModeProperty =
-        DependencyProperty.Register(nameof(GameMode), typeof(GameMode), typeof(GameResultDisplay), new PropertyMetadata(GameMode.NotRunning, OnGameModeChanged));
-
-    public ICommand? LostCommand
-    {
-        get => (ICommand?)GetValue(LostCommandProperty);
-        set => SetValue(LostCommandProperty, value);
-    }
-
-    public static readonly DependencyProperty LostCommandProperty =
-        DependencyProperty.Register(nameof(LostCommand), typeof(ICommand), typeof(GameResultDisplay), new (OnLostCommandChanged));
-
-    public Visibility LostButtonVisibility => LostCommand is not null ? Visibility.Visible : Visibility.Collapsed;
-
-    public ICommand? WonCommand
-    {
-        get => (ICommand?)GetValue(WonCommandProperty);
-        set => SetValue(WonCommandProperty, value);
-    }
-
-    public static readonly DependencyProperty WonCommandProperty =
-        DependencyProperty.Register(nameof(WonCommand), typeof(ICommand), typeof(GameResultDisplay), new (OnWonCommandChanged));
-
-    public Visibility WonButtonVisibility => WonCommand is not null ? Visibility.Visible : Visibility.Collapsed;
-
-    private static void OnGameModeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var a = (GameResultDisplay)d;
-        a.OnPropertyChanged(nameof(GameMode));
-    }
-
-    private static void OnLostCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var that = (GameResultDisplay)d;
-        that.OnPropertyChanged(nameof(LostCommand));
-        that.OnPropertyChanged(nameof(LostButtonVisibility));
-    }
-
-    private static void OnWonCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var that = (GameResultDisplay)d;
-        that.OnPropertyChanged(nameof(WonCommand));
-        that.OnPropertyChanged(nameof(WonButtonVisibility));
-    }
+    public static readonly DependencyProperty ViewModelProperty =
+        DependencyProperty.Register(nameof(ViewModel), typeof(GamePageViewModel), typeof(GameResultDisplay));
 }
