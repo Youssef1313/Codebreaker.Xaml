036fc503a37fd253c486e8a5ddbe007501965dcb
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-17 15:00:06 +0100

    Initialized CodeBreaker.Avalonia

---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace CodeBreaker.Avalonia.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace CodeBreaker.Avalonia.Views;
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
