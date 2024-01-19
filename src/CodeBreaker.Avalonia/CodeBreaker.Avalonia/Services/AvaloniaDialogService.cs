33ec605989d28c12aaf72756154a369b947e4c51
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-19 15:14:11 +0100

    Created minimal DialogService

---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Services/AvaloniaDialogService.cs
@@ -0,0 +1,14 @@
+﻿using Codebreaker.ViewModels.Contracts.Services;
+using Codebreaker.ViewModels.Services;
+using System.Threading.Tasks;
+
+namespace CodeBreaker.Avalonia.Services;
+
+internal class AvaloniaDialogService(IInfoBarService infoBarService) : IDialogService
+{
+    public Task ShowMessageAsync(string message)
+    {
+        infoBarService.New.WithMessage(message).Show();
+        return Task.CompletedTask;
+    }
+}
