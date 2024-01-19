1158210f166917083e297fbc8dddbc5c0861a4f3
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-19 15:16:59 +0100

    Added usage of ApplicationBuilder

--- src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/App.axaml.cs
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/App.axaml.cs
@@ -4,10 +4,30 @@ using Avalonia.Markup.Xaml;
 using CodeBreaker.Avalonia.ViewModels;
 using CodeBreaker.Avalonia.Views;
 using CodeBreaker.Avalonia.Views.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CodeBreaker.Avalonia;
 public partial class App : Application
 {
+    private readonly IHost _host;
+
+    public App()
+    {
+        var builder = Host.CreateApplicationBuilder(Environment.GetCommandLineArgs());
+        _host = builder.Build();
+        DefaultScope = _host!.Services.CreateScope();
+    }
+
+    internal new static App Current => (App)Application.Current!;
+
+    internal IServiceScope? DefaultScope { get; private set; }
+
+    public T GetService<T>()
+        where T : class =>
+        DefaultScope!.ServiceProvider.GetRequiredService<T>();
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
