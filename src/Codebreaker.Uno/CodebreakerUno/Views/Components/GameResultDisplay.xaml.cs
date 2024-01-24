85eb3f63b36827c7fb6bd1cb8e537f50b4942747
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-24 18:49:45 +0100

    Added GameResultDisplay control

---
+++ src/Codebreaker.Uno/CodebreakerUno/Views/Components/GameResultDisplay.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Codebreaker.ViewModels;
+
+namespace CodeBreaker.Uno.Views.Components;
+
+public sealed partial class GameResultDisplay : UserControl
+{
+    public GameResultDisplay()
+    {
+        InitializeComponent();
+    }
+
+    public GamePageViewModel ViewModel
+    {
+        get => (GamePageViewModel)GetValue(MyPropertyProperty); 
+        set => SetValue(MyPropertyProperty, value);
+    }
+
+    public static readonly DependencyProperty MyPropertyProperty =
+        DependencyProperty.Register(nameof(ViewModel), typeof(GamePageViewModel), typeof(GameResultDisplay), new PropertyMetadata(null));
+}
