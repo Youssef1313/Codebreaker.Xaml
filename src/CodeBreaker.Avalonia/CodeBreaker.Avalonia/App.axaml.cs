5db9084aff50d18994f89bd813b14c88c061ea96
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-26 16:47:20 +0100

    Created Shell Page

--- src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/App.axaml.cs
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/App.axaml.cs
@@ -32,7 +32,6 @@ public partial class App : Application
         builder.Services.AddScoped<IInfoBarService, InfoBarService>();
         builder.Services.AddTransient<IDialogService, AvaloniaDialogService>();
         builder.Services.AddScoped<GamePageViewModel>();
-        builder.Services.AddTransient<GamePage>();
         builder.Services.AddHttpClient<IGamesClient, GamesClient>(client =>
         {
             client.BaseAddress = new(builder.Configuration.GetRequired("ApiBase"));
@@ -60,12 +59,12 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow()
             {
-                Content = _host.Services.GetRequiredService<GamePage>()
+                Content = new Shell()
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
-            singleViewPlatform.MainView = new GamePage();
+            singleViewPlatform.MainView = new Shell();
         }
 
         base.OnFrameworkInitializationCompleted();
---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Views/Pages/Shell.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CodeBreaker.Avalonia;
+
+public partial class Shell : UserControl
+{
+    public Shell()
+    {
+        InitializeComponent();
+    }
+}
