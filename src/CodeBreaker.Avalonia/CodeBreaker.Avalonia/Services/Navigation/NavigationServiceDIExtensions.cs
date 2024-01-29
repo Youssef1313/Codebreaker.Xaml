79e259020757cb67595e02065c21a51ab6183553
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-29 05:14:30 +0100

    Added di extension for navigation related services

---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Services/Navigation/NavigationServiceDIExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Codebreaker.ViewModels.Contracts.Services;
+using CodeBreaker.Avalonia.Contracts.Services.Navigation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CodeBreaker.Avalonia.Services.Navigation;
+
+internal static class NavigationServiceDIExtensions
+{
+    public static IServiceCollection AddNavigation<TNavigationService>(this IServiceCollection services, Action<PageServiceBuilder> builder)
+        where TNavigationService : class, IAvaloniaNavigationService, INavigationService
+    {
+        PageServiceBuilder pageServiceBuilder = new();
+        builder(pageServiceBuilder);
+        services.AddSingleton<IPageService>(pageServiceBuilder.Build());
+        services.AddSingleton<IAvaloniaNavigationService, TNavigationService>();
+        services.AddSingleton<INavigationService>(x => x.GetRequiredService<IAvaloniaNavigationService>());
+        return services;
+    }
+}
