0a1539c9e43a2b8b377d644849ecebc7b13fe89e
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-29 05:15:58 +0100

    Added demo-button to navigate to the TestPage

--- src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Views/Pages/GamePage.axaml.cs
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Views/Pages/GamePage.axaml.cs
@@ -1,15 +1,25 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Codebreaker.ViewModels;
+using Codebreaker.ViewModels.Contracts.Services;
 
 namespace CodeBreaker.Avalonia.Views.Pages;
 
 public partial class GamePage : UserControl
 {
+    private readonly INavigationService _navigationService;
+
     public GamePage()
     {
-        DataContext = App.Current.GetService<GamePageViewModel>(); ;
+        DataContext = App.Current.GetService<GamePageViewModel>();
         InitializeComponent();
+        _navigationService = App.Current.GetService<INavigationService>();
     }
 
     public GamePageViewModel ViewModel => (GamePageViewModel)DataContext!;
+
+    private void ToTestPageButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        _navigationService.NavigateToAsync("TestPage");
+    }
 }
