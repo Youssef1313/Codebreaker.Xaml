7f74c79c926cf1fb0f63b0ebcd47fced99d53183
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-01-29 05:14:15 +0100

    Added navigation service

---
+++ src/CodeBreaker.Avalonia/CodeBreaker.Avalonia/Contracts/Services/Navigation/IAvaloniaNavigationService.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+using Codebreaker.ViewModels.Contracts.Services;
+
+namespace CodeBreaker.Avalonia.Contracts.Services.Navigation;
+
+internal interface IAvaloniaNavigationService : INavigationService
+{
+    ContentControl CurrentPage { get; }
+
+    void Initialize();
+}
